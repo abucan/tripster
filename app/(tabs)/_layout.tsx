63433caa5a413b4e3d48d4ef7a0e9f11adf3f274
Ante Bucan <ab8390@g.rit.edu>
2025-02-28 12:45:26 +0100

    updated icon for home screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,18 @@
 import { Tabs } from 'expo-router';
 
+import { AntDesign } from '@expo/vector-icons';
+
 export default function TabsLayout() {
   return (
     <Tabs>
       <Tabs.Screen
         name="home/index"
         options={{
+          title: 'Home',
           headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" color={color} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
